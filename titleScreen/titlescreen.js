bb203f46646ab7a0728a236124c276506a236c13
stockila <stockila@dukes.jmu.edu>
2026-03-31 18:02:58 -0400

    Fixed title screen sizing
    Made the title screen fit the new canvas size

--- titleScreen/titlescreen.js
+++ titleScreen/titlescreen.js
@@ -13,60 +13,60 @@ function drawTitleScreen() {
   fill(30, 50, 0);
   stroke(30, 50, 0);
   //grass
-  rect(-1, 235, 751, 200);
+  rect(-1, 235, 751, 240);
   //tree
   fill(92, 51, 23);
   stroke(92, 51, 23);
-  rect(40, 211, 12, 25);
-  rect(700, 211, 12, 25)
+  rect(27, 211, 8, 25);
+  rect(600, 211, 8, 25)
   fill(0, 80, 0);
   stroke(0, 80, 0);
-  rect(28, 170, 36, 50, 3);
-  rect(688, 170, 36, 50, 3);
+  rect(15, 170, 30, 50, 3);
+  rect(589, 170, 30, 50, 3);
   
   // castle
   strokeWeight(3);
   fill(60, 50, 25);
   
   stroke(110, 95, 42);
-  rect(75, 149, 600, 85);
+  rect(60, 149, 500, 85);
   stroke(60, 50, 25);
-  rect(75, 204, 600, 30);
-  for (let x = 75; x < 605; x += 50) {
+  rect(60, 204, 500, 30);
+  for (let x = 75; x < 505; x += 50) {
     rect(x, 136, 25, 40);
   }
 
   fill(85, 65, 30);
   stroke(110, 95, 42);
-  rect(75, 129, 70, 105);
-  rect(605, 129, 70, 105);
+  rect(60, 129, 70, 105);
+  rect(505, 129, 70, 105);
   fill(60, 50, 25);
-  rect(70, 109, 80, 20);
-  rect(600, 109, 80, 20);
+  rect(55, 109, 80, 20);
+  rect(500, 109, 80, 20);
   fill(20);
-  rect(98, 160, 24, 40, 30, 30, 0, 0);
-  rect(628, 160, 24, 40, 30, 30, 0, 0);
-  rect(330, 160, 75, 75, 50, 50, 0, 0);
+  rect(84, 160, 24, 40, 30, 30, 0, 0);
+  rect(528, 160, 24, 40, 30, 30, 0, 0);
+  rect(280, 160, 75, 75, 50, 50, 0, 0);
 
   // title bar
   fill(0);
-  rect(170, 35, 410, 90);
+  rect(115, 35, 410, 90);
   textSize(35);
   fill(110, 95, 42);
 
-  rect(180, 46, 1)
-  rect(182, 48, 1)
-  rect(178, 48, 1)
-  rect(180, 50, 1)
+  rect(123, 46, 1)
+  rect(125, 48, 1)
+  rect(121, 48, 1)
+  rect(123, 50, 1)
 
-  rect(180 + 385, 46, 1)
-  rect(182 + 385, 48, 1)
-  rect(178 + 385, 48, 1)
-  rect(180 + 385, 50, 1)
+  rect(123 + 385, 46, 1)
+  rect(125 + 385, 48, 1)
+  rect(121 + 385, 48, 1)
+  rect(123 + 385, 50, 1)
   textFont('Courier New');
-  text("SCARY TOWER", 253, 76);
+  text("SCARY TOWER", 203, 76);
   textSize(25)
-  text("D E F E N C E", 270, 110);
+  text("D E F E N C E", 220, 110);
 
   //moon
   fill(210, 195, 142);
@@ -77,48 +77,37 @@ function drawTitleScreen() {
   circle(700, 40, 50);
 
   // stars
-  strokeWeight(1);
-  fill(255, 255, 255);
-  stroke(255, 255, 255);
-  circle(50, 50, 1);
-  circle(100, 60, 1);
-  circle(150, 20, 1);
-  circle(20, 200, 1);
-  circle(10, 130, 1);
-  circle(450, 10, 1);
-  circle(350, 18, 1);
-  circle(630, 90, 1);
-  circle(730, 10, 1);
+  
 
   // buttons
 
   strokeWeight(2);
   fill(200, 150, 2);
   stroke(100, 75, 22)
-  rect(280, 170, 175, 40);
+  rect(280 - 50, 170, 175, 40);
   textSize(18)
   fill(0);
-  text("PLAY", 347, 195);
+  text("PLAY", 347 - 50, 195);
 
   
-  rect(280, 220, 70, 20);
+  rect(280 - 50, 220, 70, 20);
   textSize(10);
   fill(200);
-  text("Settings", 292, 233)
+  text("Settings", 292 - 50, 233)
 
   fill(0);
-  rect(390, 220, 65, 20)
+  rect(390 - 50, 220, 65, 20)
   fill(200);
-  text("LORE", 410, 233)
+  text("LORE", 410 - 50, 233)
 
 }
 
 function mousePressedTitleScreen(){
-  if (mouseX > 280 && mouseX < 455 && mouseY > 170 && mouseY < 210) {
+  if (mouseX > 230 && mouseX < 405 && mouseY > 170 && mouseY < 210) {
     gameState = 'levelSelect';
     return;
   }
-  if (mouseX > 280 && mouseX < 350 && mouseY > 220 && mouseY < 240) {
+  if (mouseX > 230 && mouseX < 300 && mouseY > 220 && mouseY < 240) {
     gameState = 'settings';
   }
 }
