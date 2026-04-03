132ff5c44e819a62ff31269e2a6679a9aa74447b
Evan Strong <evanstrong04@gmail.com>
2026-04-03 09:26:25 -0400

    moved game states folder from dev

---
+++ gameState/gameState.js
@@ -0,0 +1,86 @@
+let gameState = 'titleScreen'
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textFont('Courier New'); 
+  setupMap1(); 
+  genStars();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+function preload() {
+  preloadMap1();
+  grassCardSprite1 = loadImage('map1/game_background_4.png');
+  // ice sprite
+  // lava sprite
+}
+
+function draw() {
+  if (gameState === 'titleScreen'){
+    drawTitleScreen();
+  } else if (gameState === 'settings'){
+    drawSettings();
+  }else if (gameState === 'levelSelect') {
+    drawLevelSelect();
+  } else if (gameState === 'map1') {
+    drawMap1();
+  }
+    else if (gameState === 'map2'){
+      // map 2
+    }
+    else if (gameState === 'map3'){
+      // map 3
+    }
+}
+
+function mousePressed() {
+  if (gameState === 'titleScreen'){
+    mousePressedTitleScreen();
+  } else if (gameState === 'settings'){
+    mousePressedSettings();
+  }else if (gameState === 'levelSelect') {
+    mousePressedLevelSelect();
+  } else if (gameState === 'map1') {
+    mousePressedMap1();
+  }
+    else if (gameState === 'map2'){
+      // map 2
+    }
+    else if (gameState === 'map3'){
+      // map 3
+    }
+}
+
+
+// stars
+let stars = [];
+
+function genStars(){
+  for (let i = 0; i < 80; i++) {
+    stars.push({ x: random(width), y: random(height), size: 3 });
+  }
+}
+
+function drawStars(){
+
+  for (let s of stars) {
+    fill(255)
+    noStroke();
+    ellipse(s.x, s.y, s.size);
+  }
+}
+
+function drawMoon(sx, sy){
+  fill(210, 195, 142);
+  stroke(210, 195, 142);
+  circle(690*sx, 50*sy, 50*sx);
+  fill(0, 0, 30);
+  stroke(0, 0, 30);
+  circle(700*sx, 40*sy, 50*sx);
+
+  console.log('drawing moon', sx, sy)
+}
+
