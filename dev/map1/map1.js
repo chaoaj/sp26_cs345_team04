41263637a37339619f04ad71504022ca100367be
alyssamiller95 <mille9ak@dukes.jmu.edu>
2026-04-17 08:04:51 -0400

    Update map1.js

--- dev/map1/map1.js
+++ dev/map1/map1.js
@@ -55,6 +55,7 @@ grass = autoCrop(grass)
 
 }
 
+<<<<<<< Updated upstream
 
 // function drawStore() {
 //   rect(width - 350, 0, 350, height);
@@ -70,6 +71,49 @@ function drawStore() {
   text("level", width - 340, 40)
 }
 
+=======
+function setupStore() {
+  createCanvas(300, 400);
+}
+
+function drawStore1() {
+  fill(0, 0, 0, 150);
+  rect(1050, 0, 550, 1000);
+  textFont('Courier New');
+  fill('white');
+  
+  // Tower set 1
+  image(tower1, 0, 5, 85, 85);
+  image(tower2, 105, 5, 85, 85);
+  image(tower3, 210, 5, 85, 85);
+  
+  fill('white');               
+  textSize(20);      
+  text("$100", 10, 110);
+  text("$150", 115, 110);
+  text("$200", 220, 110);
+  
+  // Tower set 2
+  image(tower4, 0, 125, 85, 85);
+  image(tower6, 105, 125, 85, 85);
+  image(tower5, 210, 125, 85, 85);
+  
+  fill('white');           
+  textSize(20);     
+  text("$100", 10, 235);
+  text("$150", 115, 235);
+  text("$200", 220, 235);
+  
+  // Tower set 3
+  image(tower7, 0, 245, 85, 85);
+  image(tower8, 105, 245, 85, 85);
+  image(tower9, 210, 245, 85, 85);
+  
+  text("$100", 10, 355);
+  text("$150", 115, 355);
+  text("$200", 220, 355);
+}
+>>>>>>> Stashed changes
 
 function drawMap1() {
   sx = width / 640;
